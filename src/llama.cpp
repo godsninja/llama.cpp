d2ad1f6f119edd760e04258ce96a0e2d938d4cab
godsninja <maninthemirrorr25@gmail.com>
2025-01-13 23:22:25 +0300

    Update llama.cpp

--- src/llama.cpp
+++ src/llama.cpp
@@ -1,31 +1,3 @@
-#include "llama-impl.h"
-
-#include "llama-chat.h"
-#include "llama-mmap.h"
-#include "llama-context.h"
-#include "llama-vocab.h"
-#include "llama-sampling.h"
-#include "llama-kv-cache.h"
-#include "llama-model-loader.h"
-#include "llama-model.h"
-
-#include "ggml.h"
-#include "ggml-alloc.h"
-#include "ggml-backend.h"
-#include "ggml-cpp.h"
-
-#include <algorithm>
-#include <array>
-#include <cassert>
-#include <cfloat>
-#include <cmath>
-#include <cstddef>
-#include <cstdint>
-#include <cstdio>
-#include <cstring>
-#include <ctime>
-#include <functional>
-
 #if defined(_MSC_VER)
 #pragma warning(disable: 4244 4267) // possible loss of data
 #endif
